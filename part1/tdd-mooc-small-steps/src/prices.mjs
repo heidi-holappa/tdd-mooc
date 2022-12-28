75f552581b43d8739e5c3170abdea95cf8117cb7
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-12-28 19:26:27 +0200

    Conversion Propagation 2: make change. Tests pass.

--- part1/tdd-mooc-small-steps/src/prices.mjs
+++ part1/tdd-mooc-small-steps/src/prices.mjs
@@ -20,7 +20,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const cost = calculateCost(age, type, date, baseCost);
+    const cost = calculateCost(age, type, convert(date), baseCost);
     res.json({ cost });
   });
 
