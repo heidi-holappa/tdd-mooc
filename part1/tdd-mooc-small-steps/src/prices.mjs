0fc3fd62d5e772ff3ea580916728181d9d9f7092
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-12-28 19:16:05 +0200

    Parallel change: Finish task. Tests pass.

--- part1/tdd-mooc-small-steps/src/prices.mjs
+++ part1/tdd-mooc-small-steps/src/prices.mjs
@@ -19,16 +19,10 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parsePlainDate(req.query.date); 
+    const date = parsePlainDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
-
-  function parseDate(dateString) {
-    if (dateString) {
-      return new Date(dateString);
-    }
-  }
 
   function parsePlainDate(dateString) {
     if (dateString) {
