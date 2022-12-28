11f3a5b05b1b2c3c4de26d44f5a4e2040ee8ed69
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-12-28 19:24:40 +0200

    Conversion Propagation 2: make change. Tests pass.

--- part1/tdd-mooc-small-steps/src/prices.mjs
+++ part1/tdd-mooc-small-steps/src/prices.mjs
@@ -70,7 +70,7 @@ function createApp(database) {
 
   function calculateReduction(date) {
     let reduction = 0;
-    if (date && isMonday(convert(date)) && !isHoliday(date)) {
+    if (date && isMonday(convert(date)) && !isHoliday(convert(date))) {
       reduction = 35;
     }
     return reduction;
