2c5be59b4a7b7a769cb9a39705d6292a13b82561
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-01-12 10:15:09 +0200

    Complete initial tests for level 9 NSS

--- part2/tdd-mooc-tetris/src/NintendoScoringSystem.mjs
+++ part2/tdd-mooc-tetris/src/NintendoScoringSystem.mjs
@@ -3,6 +3,9 @@ export class NintendoScoringSystem {
     constructor() {
         this.lines = {
             1: 40,
+            2: 100,
+            3: 300,
+            4: 1200
         }
     }
 
--- part2/tdd-mooc-tetris/test/NintendoScoringSystem.test.mjs
+++ part2/tdd-mooc-tetris/test/NintendoScoringSystem.test.mjs
@@ -2,12 +2,39 @@ import { expect } from "chai";
 import { NintendoScoringSystem } from "../src/NintendoScoringSystem.mjs"
 
 describe("On Nintendo Scoring System", () => {
+  let nss;
+  beforeEach(() => {
+    nss = new NintendoScoringSystem();
+  });
 
   it("Clearing one line on level zero gives 40 points", () => {
-    let nss = new NintendoScoringSystem()
-    let points = nss.getPoints(0, 1)
-
+    let points = nss.getPoints(0, 1);
     expect(points).to.equal(40); 
+  });
+
+  it("Clearing two lines on level zero gives 100 points", () => {
+    let points = nss.getPoints(0, 2);
+    expect(points).to.equal(100); 
+
+  });
+
+  it("Clearing three lines on level zero gives 300 points", () => {
+    let points = nss.getPoints(0, 3);
+    expect(points).to.equal(300); 
+
+  });
+
+  it("Clearing four lines on level zero gives 1200 points", () => {
+    let points = nss.getPoints(0, 4);
+    expect(points).to.equal(1200); 
+
+  });
+
+  it("Clearing 4 lines on level 9 gives 12000 points", () => {
+    let level = 9
+    let lines = 4
+    let points = nss.getPoints(level, lines);
+    expect(points).to.equal(12000); 
   });
 
 });
