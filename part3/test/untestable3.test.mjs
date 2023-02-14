d9329aa2230178e467a70026ce121d0154062272
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-02-14 16:36:45 +0200

    First test for untestable4

--- part3/test/untestable3.test.mjs
+++ part3/test/untestable3.test.mjs
@@ -1,7 +1,7 @@
-import { expect } from "chai";
-import { parsePeopleCsv } from "../src/testable3.mjs";
-import { writeFile } from "node:fs/promises";
-import { unlink } from "node:fs/promises";
+import { expect } from "chai"
+import { parsePeopleCsv } from "../src/testable3.mjs"
+import { writeFile } from "node:fs/promises"
+import { unlink } from "node:fs/promises"
 
 // example input:
 // Loid,Forger,,Male
@@ -9,43 +9,36 @@ import { unlink } from "node:fs/promises";
 // Yor,Forger,27,Female
 
 describe("Untestable-to-testable 3: CSV file parsing", () => {
-  let testFilename = "people.test.csv";
+  let testFilename = "people.test.csv"
   beforeEach(() => {
-    let data = `Loid,Forger,,Male\nAnya,Forger,6,Female\nYor,Forger,27,Female`;
+    let data = `Loid,Forger,,Male\nAnya,Forger,6,Female\nYor,Forger,27,Female`
     writeFile(testFilename, data, (err) => {
-      if (err) throw err;
+      if (err) throw err
       else {
-        console.log("SUCCESS");
+        console.log("SUCCESS")
       }
-    });
-  });
-
-  xit("todo", async () => {
-    // TODO: write proper tests
-    try {
-      expect(await parsePeopleCsv(testFilename)).to.deep.equal([]);
-    } catch (e) {}
-  });
+    })
+  })
 
   it("Returns correct amount of persons", async () => {
-    let data;
+    let data
     try {
-      data = await parsePeopleCsv(testFilename);
+      data = await parsePeopleCsv(testFilename)
     } catch (err) {
       console.log(
         `FAILURE: DETAILS BELOW
         ${err}`
-      );
+      )
     }
-    let persons = data.length;
-    expect(persons).to.equal(3);
-  });
+    let persons = data.length
+    expect(persons).to.equal(3)
+  })
 
   afterEach(() => {
     try {
-      unlink(testFilename);
+      unlink(testFilename)
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  });
-});
+  })
+})
--- part3/test/untestable4.test.mjs
+++ part3/test/untestable4.test.mjs
@@ -1,16 +1,27 @@
-import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
+import { PasswordService, PostgresUserDao } from "../src/testable4.mjs"
+import { expect } from "chai"
+import argon2 from "@node-rs/argon2"
 
 describe("Untestable 4: enterprise application", () => {
-  let service;
+  let service
+  let dao
   beforeEach(() => {
-    service = new PasswordService();
-  });
+    service = new PasswordService()
+    dao = new PostgresUserDao()
+  })
 
   afterEach(() => {
-    PostgresUserDao.getInstance().close();
-  });
+    PostgresUserDao.getInstance().close()
+  })
 
-  it("todo", async () => {
+  xit("todo", async () => {
     // TODO: write proper tests
-  });
-});
+  })
+
+  it("password can be changed", async () => {
+    const password = argon2.hashSync("salasana")
+    dao.save([1, password])
+    const result = service.changePassword(1, "salasana", "salasana")
+    expect(result).to.not.equal(password)
+  })
+})
