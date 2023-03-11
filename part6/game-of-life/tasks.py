dcf4a1372f6bc485c782bc3a83b1b0456aa4113b
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-03-11 20:42:57 +0200

    add arguments to invoke start

--- part6/game-of-life/tasks.py
+++ part6/game-of-life/tasks.py
@@ -7,3 +7,15 @@ def test(ctx):
 @task
 def coverage_test(ctx):
     ctx.run("coverage run -m pytest src/tests/")
+
+@task(
+    optional=['exp_file'],
+    help={
+        'in-file': 'Name and subdirectory of the importable rle-file',
+        'cycles': 'Number of life cycles to iterate through'
+    }
+)
+def start(ctx, in_file, cycles):
+    """A command to start the application. Requires two arguments.
+    """
+    ctx.run(f"python3 src/index.py --imp {in_file} --iter {cycles}")
