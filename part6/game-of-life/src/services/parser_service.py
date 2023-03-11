349baf7987ca1f114db219e151771c88f3474cd7
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-03-11 20:41:44 +0200

    Add feature for creating a str from grid

--- part6/game-of-life/src/services/parser_service.py
+++ part6/game-of-life/src/services/parser_service.py
@@ -1,10 +1,15 @@
+import os
 class ParserService:
 
     def __init__(self):
         self.x_dim = 0
         self.y_dim = 0
         self.pattern_as_str = ""
+        self.str_pattern_from_grid = ""
         self.pattern_as_grid = []
+        self.import_subdir_and_filename = ""
+        self.export_filename = ""
+        self.iterations = 0
 
     def create_grid_from_pattern(self):
         """
@@ -39,6 +44,44 @@ class ParserService:
                     str_idx += 1
                     break
 
+    def create_str_pattern_from_grid(self):
+        y_count = 0
+        x_count = 0
+        new_pattern = ""
+        for y in range(self.y_dim):
+            for x in range(self.x_dim):
+                if x == 0:
+                    if self.pattern_as_grid[y][x] == 1:
+                        x_count = 1
+                    else:
+                        y_count = 1
+                elif self.pattern_as_grid[y][x] == 1:
+                    if self.pattern_as_grid[y][x-1] != 1:
+                        count = ""
+                        if y_count > 1:
+                            count = str(y_count)
+                        new_pattern += f"{count}b"
+                        y_count = 0
+                    x_count += 1
+                else:
+                    if self.pattern_as_grid[y][x-1] != 0:
+                        count = ""
+                        if x_count > 1:
+                            count = str(x_count)
+                        new_pattern += f"{count}o"
+                        x_count = 0
+                    y_count += 1
+            if x_count:
+                if x_count > 1:
+                    new_pattern += f"{str(x_count)}o"
+                else:
+                    new_pattern += "o"
+            x_count = 0
+            y_count = 0
+            new_pattern +="$"
+        self.str_pattern_from_grid = f"{new_pattern[:-1]}!"
+
+
     def parse_file(self, file: str):
         with open(file, "r", encoding="utf-8") as rle_file:
             content = rle_file.read()
@@ -66,3 +109,9 @@ class ParserService:
                     file.write(split_pattern[i])
             file.close()
 
+    def execute_cli_args(self, path_and_file: str, iterations: int):
+        self.import_subdir_and_filename = path_and_file
+        self.iterations = iterations
+        self.export_filename = f"{path_and_file[:-4]}-iterated-{iterations}-life-cycles.rle"
+        import_directory_and_filename = os.path.join(os.path.dirname(__file__), '..', '..', 'rle-files/', self.import_subdir_and_filename)
+        self.parse_file(import_directory_and_filename)
