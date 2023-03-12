04dd904ced957d512cc5a5ff9f55e90345eefa2a
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-03-12 17:19:20 +0200

    Format code

--- part6/game-of-life/src/services/service_controller.py
+++ part6/game-of-life/src/services/service_controller.py
@@ -1,6 +1,7 @@
 from services.parser_service import ParserService
 from services.game_service import GameService
 
+
 class ServiceController:
     def __init__(self):
         self.parser = ParserService()
@@ -11,7 +12,8 @@ class ServiceController:
     def execute(self, subdir_and_filename: str, iterations: int):
         self.subdir_and_filename = subdir_and_filename
         self.iterations = iterations
-        self.parser.initialize_parser_with_given_cli_args(subdir_and_filename, iterations)
-        iterated_grid, r_value = self.game_service.iterate_n_ticks(self.parser.pattern_as_grid, self.iterations, self.parser.initial_r_value)
+        self.parser.initialize_parser_with_given_cli_args(
+            subdir_and_filename, iterations)
+        iterated_grid, r_value = self.game_service.iterate_n_ticks(
+            self.parser.pattern_as_grid, self.iterations, self.parser.initial_r_value)
         self.parser.create_rle_file(iterated_grid, r_value)
-        
