cbd3efde7254cffbcc8e64ce51fa5963dd802fa0
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-03-12 17:19:08 +0200

    Fix issue with export-directory, format code

--- part6/game-of-life/src/services/parser_service.py
+++ part6/game-of-life/src/services/parser_service.py
@@ -1,4 +1,6 @@
 import os
+
+
 class ParserService:
 
     def __init__(self):
@@ -14,32 +16,37 @@ class ParserService:
 
     def create_grid_from_pattern(self):
         """
-            Note: cell_multiplier is tuple that keeps track of whether the multiplier has been updated.
-            It it has been updated, then any additional integers will be concatenated with the existing string
-            When the grid is updated, the cell_multiplier tuple will be reset. 
+            Note: cell_multiplier is tuple that keeps track of
+            whether the multiplier has been updated. It it has
+            been updated, then any additional integers will be
+            concatenated with the existing string When the grid
+            is updated, the cell_multiplier tuple will be reset.
         """
-        self.pattern_as_grid = [[0]* self.x_dim for _ in range(self.y_dim)]
+        self.pattern_as_grid = [[0] * self.x_dim for _ in range(self.y_dim)]
         str_idx = 0
-        for y in range(self.y_dim):
-            x = 0
+        for y_coord in range(self.y_dim):
+            x_coord = 0
             cell_multiplier = ("1", False)
-            while x < self.x_dim:
+            while x_coord < self.x_dim:
                 if self.pattern_as_str[str_idx] == "!":
                     break
                 if self.pattern_as_str[str_idx].isdigit():
                     if not cell_multiplier[1]:
                         cell_multiplier = (self.pattern_as_str[str_idx], True)
                     else:
-                        cell_multiplier = (cell_multiplier[0] + self.pattern_as_str[str_idx], True)
+                        cell_multiplier = (
+                            cell_multiplier[0] + self.pattern_as_str[str_idx], True)
                     str_idx += 1
-                    print(f"str_idx: {str_idx}, cell-multiplier: {cell_multiplier}")
+                    print(
+                        f"str_idx: {str_idx}, cell-multiplier: {cell_multiplier}")
                     continue
                 if self.pattern_as_str[str_idx] == "o" or self.pattern_as_str[str_idx] == "b":
                     cell_value = int(bool(self.pattern_as_str[str_idx] == "o"))
                     x_length = int(cell_multiplier[0])
-                    self.pattern_as_grid[y][x:x + x_length] = [cell_value]*x_length
+                    self.pattern_as_grid[y_coord][x_coord:x_coord +
+                                                  x_length] = [cell_value]*x_length
                     str_idx += 1
-                    x += x_length
+                    x_coord += x_length
                     cell_multiplier = ("1", False)
                 if self.pattern_as_str[str_idx] == "$":
                     str_idx += 1
@@ -49,15 +56,15 @@ class ParserService:
         y_count = 0
         x_count = 0
         new_pattern = ""
-        for y in range(self.y_dim):
-            for x in range(self.x_dim):
-                if x == 0:
-                    if iterated_pattern_as_grid[y][x] == 1:
+        for y_coord in range(self.y_dim):
+            for x_coord in range(self.x_dim):
+                if x_coord == 0:
+                    if iterated_pattern_as_grid[y_coord][x_coord] == 1:
                         x_count = 1
                     else:
                         y_count = 1
-                elif iterated_pattern_as_grid[y][x] == 1:
-                    if iterated_pattern_as_grid[y][x-1] != 1:
+                elif iterated_pattern_as_grid[y_coord][x_coord] == 1:
+                    if iterated_pattern_as_grid[y_coord][x_coord-1] != 1:
                         count = ""
                         if y_count > 1:
                             count = str(y_count)
@@ -65,7 +72,7 @@ class ParserService:
                         y_count = 0
                     x_count += 1
                 else:
-                    if iterated_pattern_as_grid[y][x-1] != 0:
+                    if iterated_pattern_as_grid[y_coord][x_coord-1] != 0:
                         count = ""
                         if x_count > 1:
                             count = str(x_count)
@@ -79,9 +86,8 @@ class ParserService:
                     new_pattern += "o"
             x_count = 0
             y_count = 0
-            new_pattern +="$"
+            new_pattern += "$"
         self.str_pattern_from_grid = f"{new_pattern[:-1]}!"
-
 
     def parse_file(self, file: str):
         with open(file, "r", encoding="utf-8") as rle_file:
@@ -107,7 +113,8 @@ class ParserService:
             file.write(f"#R {str(r_value[0])} {str(r_value[1])}\n")
             file.write(f"x =  {self.x_dim}, y = {self.y_dim}\n")
             pattern_max_line_length = 70
-            split_pattern = [self.pattern_as_str[i: i + pattern_max_line_length] for i in range(0, len(self.pattern_as_str), pattern_max_line_length)]
+            split_pattern = [self.str_pattern_from_grid[i: i + pattern_max_line_length]
+                             for i in range(0, len(self.str_pattern_from_grid), pattern_max_line_length)]
             for i in range(len(split_pattern)):
                 if i + 1 < len(split_pattern):
                     file.write(split_pattern[i] + "\n")
@@ -118,7 +125,13 @@ class ParserService:
     def initialize_parser_with_given_cli_args(self, path_and_file: str, iterations: int):
         self.import_subdir_and_filename = path_and_file
         self.iterations = iterations
-        self.export_filename = f"{path_and_file[:-4]}-iterated-{iterations}-life-cycles.rle"
-        import_directory_and_filename = os.path.join(os.path.dirname(__file__), '..', '..', 'rle-files/', self.import_subdir_and_filename)
+        self.export_filename = os.path.join(
+            os.path.dirname(__file__), '..', '..', 'rle-files/',
+            f"{path_and_file[:-4]}-iterated-{iterations}-life-cycles.rle"
+        )
+        import_directory_and_filename = os.path.join(
+            os.path.dirname(__file__), '..', '..', 'rle-files/',
+            self.import_subdir_and_filename
+        )
         self.parse_file(import_directory_and_filename)
         self.create_grid_from_pattern()
